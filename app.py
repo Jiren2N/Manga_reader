ba1f2cc76bc53180f68c2f16009effeb2b678ff2
Mohit <mohit181200@gmail.com>
2025-07-09 19:02:31 +0530

    use of env for cred

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 user=os.getenv("Mohit")
 password=os.getenv("Mohit@1812")
 
-safe_user = quote_plus(str(user))
-safe_pass = quote_plus(str(password))
+safe_user = quote_plus(os.environ.get("MONGO_USER", ""))
+safe_pass = quote_plus(os.environ.get("MONGO_PASS", ""))
+
 
 MONGO_URI = f"mongodb+srv://{safe_user}:{safe_pass}@cluster0.jhusqxq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(MONGO_URI)
